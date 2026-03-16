30eef21f80c576798b2eeb103268d783c66a3f2b
marioprietodev <marioprietodev@gmail.com>
2026-03-16 15:24:36 +0100

    opcion de borrado de cliente

--- src/main/java/com/proyectodam/demo/controller/ViewController.java
+++ src/main/java/com/proyectodam/demo/controller/ViewController.java
@@ -4,10 +4,12 @@ import com.proyectodam.demo.model.Cliente;
 import com.proyectodam.demo.repository.CitaRepository;
 import com.proyectodam.demo.repository.ClienteRepository;
 import com.proyectodam.demo.repository.ServicioRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -59,5 +61,19 @@ public class ViewController {
             return "index"; // redirigimos al a tabla para comprobar
         }
 
+    }
+    @Transactional
+    @GetMapping("/cliente/eliminar/{id}")
+    public String eliminarCliente (@PathVariable("id")long id){
+        try {
+            clienteRepository.deleteById(id);
+            System.out.println("Cliente borrado con exito");
+        } catch (Exception e){
+            e.printStackTrace();
+            System.out.println("No se pudo borrar al cliente");
+        }
+
+
+        return "redirect:/cliente-web";
     }
 }
